c71a7877e37f24d890508d55387dc226d4b86ce8
Brian Ong <142870466+brian-ong470@users.noreply.github.com>
2024-04-24 21:23:17 +1000

    Update ErrorPage.jsx

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -5,8 +5,9 @@ const ErrorPage = () => {
   return (
     <section className='error-page'>
       <div className='center'>
-        <Link to='/' classname="btn primary">Go back Home</Link>
-        <h2>Page Not Found</h2>
+        <Link to='/' classname="btn primary">Click to Start your Journey</Link>
+        <h2>Welcome to My Blog Websited</h2>
+        <h4>All copyrights belongs to Brian Ong</h4>
       </div>
     </section>
   )
